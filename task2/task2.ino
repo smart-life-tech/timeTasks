d7a3dacf94d524a4470fc9df5a97e4f74ede12ba
smart-life-tech <christlightworld@gmail.com>
2023-10-26 16:34:15 +0100

    modified:   task2/task2.ino

--- task2/task2.ino
+++ task2/task2.ino
@@ -1,44 +1,44 @@
 /*
  here's how I approached and completed each task:
 
-**Task 1: Defining Constants**
+Task 1: Defining Constants
 In Task 1, the goal is to replace numeric literals in the code with named constants to make the code more robust and readable. Here's what I did:
 
-- I identified the numeric literals in the code. These are the values that are used directly in calculations or comparisons, such as `3600` and `60`.
+- I identified the numeric literals in the code. These are the values that are used directly in calculations or comparisons, such as 3600 and 60.
 
-- I decided to create named constants for these values to make the code more readable and maintainable. For example, I created `const int SECONDS_IN_HOUR = 3600;` and `const int SECONDS_IN_MINUTE = 60;` to represent these values.
+- I decided to create named constants for these values to make the code more readable and maintainable. For example, I created const int SECONDS_IN_HOUR = 3600; and const int SECONDS_IN_MINUTE = 60; to represent these values.
 
 - I replaced the numeric literals with the new constants in the code.
 
-**Task 2: Updating Software Design to Use Functions**
+Task 2: Updating Software Design to Use Functions
 Task 2 involves planning and designing functions that will be useful in the code. Here's how I approached this task:
 
 - I identified parts of the code where common tasks are performed, such as reading an integer from the Serial port and converting hours, minutes, and seconds to total seconds.
 
 - I thought about what parameters these functions would need (if any) and what they should return. For example,
-the `readIntegerInput` function needs a prompt as a parameter and returns an integer.
-The `convertToSeconds` function takes hours, minutes, and seconds as parameters and returns the total seconds.
+the readIntegerInput function needs a prompt as a parameter and returns an integer.
+The convertToSeconds function takes hours, minutes, and seconds as parameters and returns the total seconds.
 
 - I didn't modify the code at this stage but focused on designing the functions and specifying their parameters and return values.
 
-**Task 3: Implementing Functions in Code**
+Task 3: Implementing Functions in Code
 Task 3 involves actually writing the code for the functions designed in Task 2.
  Here's what I did to implement the functions:
 
-- I created functions with the specified names and parameters. For example, I created the `readIntegerInput`
+- I created functions with the specified names and parameters. For example, I created the readIntegerInput
  function that takes a prompt as a parameter and returns an integer.
 
-- In the `setup` function, I replaced the code for reading input from the Serial port with calls to the
- `readIntegerInput` function. This reduced the duplication of code and made it more modular.
+- In the setup function, I replaced the code for reading input from the Serial port with calls to the
+ readIntegerInput function. This reduced the duplication of code and made it more modular.
 
-- I also implemented the `convertToSeconds` function to calculate the total
+- I also implemented the convertToSeconds function to calculate the total
 seconds from hours, minutes, and seconds.
 
-- In the `setup` function, I replaced the previous calculations and input reading with calls to
-these new functions. This made the `setup` function more concise and easier to read.
+- In the setup function, I replaced the previous calculations and input reading with calls to
+these new functions. This made the setup function more concise and easier to read.
 
 - I continued to use a loop to handle multiple events,
-and I printed the results using the `printEventTime` function.
+and I printed the results using the printEventTime function.
 
 By breaking down the code into functions and using constants, the code becomes more organized,
  easier to read, and less prone to errors. It also makes the code more modular and reusable, which
